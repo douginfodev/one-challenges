94048d2fbaddd5dbbcc8af8066f9cd8996fdf883
= <=>
2024-01-23 16:03:58 -0300

    feat: Finalized Version 1.0

--- challenge01/assets/js/challenge01.js
+++ challenge01/assets/js/challenge01.js
@@ -1,29 +1,35 @@
 
 const cripto = ['e', 'i', 'a', 'o', 'u'];
 const decripto = ['enter', 'imes', 'ai', 'ober', 'ufat'];
-
-let textoCompleto = document.getElementById('textareainfo');
-let aside = document.getElementById('aside');
-let asideCenter   = document.getElementById('aside-center');
-let asideTexto    = document.getElementById('aside-text');
-
 let textoLimpo = '';
 
+// DOM
+let textoCompleto = document.getElementById('textareainfo');
+let aside = document.getElementById('aside');
+let buttonCopy = document.getElementById('btn-copy');
+let asideCenter = document.getElementById('aside-center');
+let asideTexto = document.getElementById('aside-text');
+textoCompleto.focus;
+
+// FUNCTION
 function checkText(tipo) {
     switch (tipo) {
         case 'D':
+            buttonCopy.style.display = 'flex';
+            buttonCopy.style.justifyContent = 'end';
             aside.style.display = 'flex';
             aside.style.justifyContent = 'start';
             asideCenter.style.display = 'none';
-            asideTexto.style.display  = 'flex';
+            asideTexto.style.display = 'flex';
             textoLimpo = Characters(textoCompleto.value);
             asideTexto.innerHTML = descriptografar(textoLimpo);
             break;
         case 'E':
+            buttonCopy.style.display = 'flex';
             aside.style.display = 'flex';
             aside.style.justifyContent = 'start';
             asideCenter.style.display = 'none';
-            asideTexto.style.display  = 'flex';
+            asideTexto.style.display = 'flex';
             textoLimpo = Characters(textoCompleto.value);
             asideTexto.innerHTML = criptografar(textoLimpo);
             break;
@@ -61,4 +67,14 @@ function Characters(content) {
     const parsed = str.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z\s])/g, '');
 
     return parsed.toLocaleLowerCase();
+}
+
+function copyText() {
+    let clipText = document.getElementById("aside-text").innerHTML;
+    navigator.clipboard
+        .writeText(clipText)
+        .then(() => {
+            alert("Texto Copiado");
+        }
+        );
 }
